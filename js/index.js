b5fa0af4d0d69803cca91270365b25e9042257e5
Serena Siliberti <serena.siliberti@gmail.com>
2024-01-31 17:16:14 +0100

    miglioramento

--- js/index.js
+++ js/index.js
@@ -4,38 +4,38 @@ const handleSubmit = function (e) {
 };
 
 const addTask = function () {
-  const tasks = document.querySelector("#tasks");
   const inputField = document.querySelector("#newTask input");
-  const newTask = `
-      <div class="task">
-        <span class="task-name">${inputField.value}</span>
-        <button class="delete">
-          <i class="far fa-trash-alt"></i>
-        </button>
-      </div>
-    `;
-  tasks.insertAdjacentHTML("beforeend", newTask);
-  inputField.value = "";
-  attachComplete();
-  attachDelete();
+  const taskName = inputField.value.trim();
+
+  if (taskName !== "") {
+    const newTask = createTaskElement(taskName);
+    const tasks = document.querySelector("#tasks");
+    tasks.appendChild(newTask);
+    inputField.value = "";
+  }
 };
 
-const attachComplete = function () {
-  const allTasks = document.querySelectorAll(".task");
-  allTasks.forEach((task) => {
-    task.addEventListener("click", function () {
-      this.classList.toggle("completed");
-    });
-  });
-};
+const createTaskElement = function (taskName) {
+  const task = document.createElement("div");
+  task.classList.add("task");
 
-const attachDelete = function () {
-  const allDeleteButtons = document.querySelectorAll(".delete");
-  allDeleteButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      this.parentNode.remove();
-    });
+  const taskNameSpan = document.createElement("span");
+  taskNameSpan.textContent = taskName;
+  task.appendChild(taskNameSpan);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete");
+  deleteButton.innerHTML = '<i class="far fa-trash-alt"></i>';
+  deleteButton.addEventListener("click", function () {
+    task.remove();
   });
+  task.appendChild(deleteButton);
+
+  task.addEventListener("click", function () {
+    task.classList.toggle("completed");
+  });
+
+  return task;
 };
 
 window.onload = function () {
